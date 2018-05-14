3ab1e8c1e12a8cbe25127c22417ad99756587304
Brian Foley <bfoley-teamug@users.noreply.github.com>
2018-05-14 05:34:31 -0700

    tweak

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,7 @@ passport.deserializeUser((id, done) => {
   User.findById(id)
     .then(user => {
       done(null, user);
-    }); 
+    });
 });
 
 passport.use(
@@ -21,7 +21,8 @@ passport.use(
   {
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true 
   },
   (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleId: profile.id })
